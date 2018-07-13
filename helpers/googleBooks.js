06def85779a088f0f804c8db7e134782e8e3047a
Martin <MartinGlyer@gmail.com>
2018-07-13 14:12:12 -0400

    no helper, server talks to api

---
+++ helpers/googleBooks.js
@@ -0,0 +1,1 @@
+const APIKEY = require('./../config.js')
--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ let server = express()
 //middleware and helper functions
 const parser = require('body-parser')
 const axios = require('axios')
+const APIKEY = require('../config.js')
 
 
 //hookup functionality
@@ -15,7 +16,12 @@ server.use(express.static(__dirname + '/../client/dist'));
 server.post('/', (req, res) => {
     let genre = req.body.genre
     let era = req.body.era
-    res.send(genre)
+    axios.get(`https://www.googleapis.com/books/v1/volumes?q=subject:${genre}&KEY=${APIKEY}`)
+         .then((response) => {
+            //  console.log('data received from google! ', response.data)
+             res.send(response.data.items)
+         })
+         .catch((err) => console.error(err))
 })
 
 server.get('/faves', (req, res) => {

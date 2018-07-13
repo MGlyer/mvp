b28618c16747833c503b632810843e7abdb090ba
Martin <MartinGlyer@gmail.com>
2018-07-13 11:31:09 -0400

    basic front end wirework

---
+++ client/src/index.jsx
@@ -0,0 +1,24 @@
+import Reach from 'react'
+import ReactDom from 'react-dom'
+import axios from 'axios'
+
+class App extends React.Component {
+    constructor(props) {
+        super(props)
+        this.State = {
+            bookToShow: {}
+        }
+    }
+
+
+
+    render() {
+        return (
+            <div class='main'>
+                look at me!  I'm live on the server!
+            </div>
+        )
+    }
+}
+
+ReactDom.render( <App />, document.getElementById('app') )

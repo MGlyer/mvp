18d6ba7a349120af490014876cea2411b5034931
Martin <MartinGlyer@gmail.com>
2018-07-13 18:46:52 -0400

    enables conditional rendering

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -6,14 +6,16 @@ const demo = require('../../data.js').books
 class App extends React.Component {
     constructor(props) {
         super(props)
-        this.State = {
+        this.state = {
             bookToShow: demo[0],
-            genre: ''
+            genre: '',
+            showingFaves: false
         }
         this.findBook = this.findBook.bind(this)
         this.getFaves = this.getFaves.bind(this)
         this.handleGenreSelect = this.handleGenreSelect.bind(this)
         this.handleMainClick = this.handleMainClick.bind(this)
+        this.handleFavesClick = this.handleFavesClick.bind(this)
     }
 
     // *********method section
@@ -57,48 +59,64 @@ class App extends React.Component {
 
     handleMainClick() {
         console.log('clicked the main button!')
-        this.setState({bookToShow: {}})
+        this.setState({showingFaves: true})
+        this.forceUpdate()
     }
 
     handleFavesClick() {
         console.log('clicked the faves button!')
-        this.setState({bookToShow: {}})
-        this.getFaves()
+        this.setState({showingFaves: false})
+        this.forceUpdate()
+        // this.getFaves()
     }
 
 
 
-    //*********render section
+
+
     render() {
-        return (
-            <div className='main'>
-                <h3>Look-a-Book</h3>
-                Please select a genre, and I'll find you a book!
+        if (this.state.showingFaves) {
+            return (
                 <div>
                     <button className = "mainScreen" onClick = {this.handleMainClick}>Main</button>
                     <button className = "showFaves" onClick = {this.handleFavesClick}>Show Favorited Books</button>
+                    <div> here are the faaaaves!</div>
                 </div>
-
-                <div className="dropdown">
-                    <select onClick = {this.handleGenreSelect}>
-                        <option value=''></option>
-                        <option value='History' >History</option>
-                        <option value='Romance'>Romance</option>
-                        <option value='Fantasy'>Fantasy</option>
-                        <option value='Sci-Fi'>Sci-Fi</option>
-                        <option value='Biography'>Biography</option>
-                        <option value='Travel'>Travel</option>
-                    </select>
-                    <button onClick= {this.findBook}>Find my book!</button>
-                    <button className="favorite">Save this book!</button>
+            )
+        } else {
+            return (
+                <div>
+                    <h3>Look-a-Book</h3>
+                    <span>Please select a genre, and I'll find you a book!</span>
+                    <div>
+                        <button className = "mainScreen" onClick = {this.handleMainClick}>Main</button>
+                        <button className = "showFaves" onClick = {this.handleFavesClick}>Show Favorited Books</button>
+                    </div>
+                
+                    <div className="dropdown">
+                        <select onClick = {this.handleGenreSelect}>
+                            <option value=''></option>
+                            <option value='History' >History</option>
+                            <option value='Romance'>Romance</option>
+                            <option value='Fantasy'>Fantasy</option>
+                            <option value='Sci-Fi'>Sci-Fi</option>
+                            <option value='Biography'>Biography</option>
+                            <option value='Travel'>Travel</option>
+                        </select>
+                        <button onClick= {this.findBook}>Find my book!</button>
+                        <button className="favorite">Save this book!</button>
+                    </div>
+                
+                    <div className="bookInfo">
+                
+                    </div>
+                    <div> No Faves FOR YOU </div>
                 </div>
-
-                <div className="bookInfo">
-
-                </div>
-            </div>
-        )
+            )
+        }
     }
 }
+
+
 
 ReactDom.render( <App />, document.getElementById('app') )
